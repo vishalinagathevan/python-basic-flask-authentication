d62e8bdb1e6399da6bf344c5bd537aa4781c358e
vishalinagathevans <vishaliv3635@gmail.com>
2023-07-05 12:32:10 +0530

    Python Basic Flask Authentication

---
+++ basicauth.py
@@ -0,0 +1,32 @@
+# Author: Vishali,Solverscamp
+# Date :5/7/2023
+# Description:With basic authendication.
+
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_httpauth import HTTPBasicAuth
+
+app = Flask(__name__)
+api = Api(app, prefix="/api/v1")
+auth =HTTPBasicAuth()
+
+# Basic authendication - Username ,Password
+USER_DATA ={
+    "admin":"SuperSecretPwd"
+    }
+
+@auth.verify_password
+def verify(username, password):
+    if not (username and password):
+        return False
+    return USER_DATA.get(username)==password
+
+class PrivateResource(Resource):
+    @auth.login_required
+    def get(self):
+        return {"meaning_of_life":42}
+    
+api.add_resource(PrivateResource, '/private')    
+
+if __name__=='__main__':
+    app.run(debug=True)     
